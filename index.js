c3af5b3361872cdcba0e4111a9600c6cc99f9577
James <jamesorange28@gmail.com>
2025-11-28 15:50:40 +0000

    Fix DVSA scope + remove API_CLIENT_ID

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ async function getToken() {
         client_id: process.env.CLIENT_ID,
         client_secret: process.env.CLIENT_SECRET,
         grant_type: "client_credentials",
-        scope: `api://${process.env.API_CLIENT_ID}/.default`
+        scope: "https://tapi.dvsa.gov.uk/.default"
     });
 
     const res = await axios.post(tokenUrl, data, {
