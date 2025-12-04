4dabc03ad289690a197b38bebbaeddec00d0fa24
James <jamesorange28@gmail.com>
2025-12-04 18:52:09 +0000

    loginauth

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ dotenv.config();
 
 // Routers
 import authRouter from "./routes/auth.js";
+import garageRouter from "./routes/garage.js";
 
 // DB
 import { query } from "./db/db.js";
@@ -18,6 +19,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use("/api/garage", garageRouter);
 
 // =============================
 //   DATABASE TEST ENDPOINT
