9d2cc64a0fa823cd363399bd31daad73d11f31b3
James <jamesorange28@gmail.com>
2026-01-19 17:39:06 +0000

    premium

--- services/iap/google.js
+++ services/iap/google.js
@@ -1,33 +1,6 @@
-import { google } from "googleapis";
-
-const auth = new google.auth.GoogleAuth({
-  credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON),
-  scopes: ["https://www.googleapis.com/auth/androidpublisher"],
-});
-
-export async function verifyGooglePurchase({
-  packageName,
-  productId,
-  purchaseToken,
-}) {
-  const client = await auth.getClient();
-  const androidpublisher = google.androidpublisher({
-    version: "v3",
-    auth: client,
-  });
-
-  const res = await androidpublisher.purchases.products.get({
-    packageName,
-    productId,
-    token: purchaseToken,
-  });
-
-  if (res.data.purchaseState !== 0) {
-    return { valid: false };
-  }
-
+export async function verifyGooglePurchase() {
   return {
-    valid: true,
-    purchase: res.data,
+    valid: false,
+    error: "Google Play validation not enabled yet",
   };
 }
